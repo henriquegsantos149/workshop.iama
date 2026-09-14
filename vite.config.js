e9ebdefa1a29408524a4f2c330d16d0a573b6cc4
Henrique <henrique@ambientalpro.com.br>
2026-09-14 12:11:36 -0300

    build: add matricula/index.html to vite config rollup inputs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        matricula: resolve(__dirname, 'matricula/index.html')
+      }
+    }
   }
 });
